736b073083da31df4dfe86d62fa47494318d861b
rliang <ricardoliang@gmail.com>
2016-10-15 19:56:51 -0300

    bugfix scrambling window geometries

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,14 +1,19 @@
-[
-  'clientAdded',
-  'clientRemoved',
-  'clientActivated',
-  'desktopPresenceChanged',
-].forEach(function(s) {
-  workspace[s].connect(function() {
-    workspace.desktops = (workspace.clientList().reduce(function(n, c) {
-      if (c.desktop >= 0)
-        n[c.desktop - 1] = true
-      return n
-    }, []).length || 1) + 1
+function refresh() {
+  workspace.clientList().forEach(function(win) {
+    win.savedGeometry = win.geometry
   })
-})
+  workspace.desktops = workspace.clientList().reduce(function(d, win) {
+    if (win.desktop >= 0)
+      d[win.desktop] = true
+    return d
+  }, [,]).length
+  workspace.clientList().forEach(function(win) {
+    if (win.savedGeometry)
+      win.geometry = win.savedGeometry
+  })
+}
+
+workspace.clientAdded.connect(refresh)
+workspace.clientRemoved.connect(refresh)
+workspace.clientActivated.connect(refresh)
+workspace.desktopPresenceChanged.connect(refresh)
